19c1c5a8ee56063fe2dd7a25e405a3c56c9eb54a
jeremy1990 <miroslav1990@gmail.com>
2017-11-04 23:46:28 +0800

    Add loader and sorter for pNovo results

---
+++ resultLoader.py
@@ -0,0 +1,113 @@
+'''
+This method goes through a pNovo result file and
+loads all PSMs (Peptide Spectrum Matches) into an array.
+Each item in the array is a PSM hash.
+
+A typical result block which contains one PSM in pNovo is like this
+S16	H1.87.87.2.dta	759.400500
+P1	TKPEWV	5.518731	1.000000	-1	-4.121707	5.518731
+P2	TKEPWV	5.518731	1.000000	-1	-4.121707	5.518731
+P3	KTPEWV	5.518731	1.000000	-1	-4.121707	5.518731
+P4	KTEPWV	5.518731	1.000000	-1	-4.121707	5.518731
+P5	TKPERE	5.244681	1.000000	-3	-1.307918	5.244681
+P6	TKPEER	5.244681	1.000000	-3	-1.307918	5.244681
+P7	TKEPRE	5.244681	1.000000	-3	-1.307918	5.244681
+P8	TKEPER	5.244681	1.000000	-3	-1.307918	5.244681
+P9	KTPERE	5.244681	1.000000	-3	-1.307918	5.244681
+P10	KTPEER	5.244681	1.000000	-3	-1.307918	5.244681
+
+time:	0.003000
+
+"S{$specNum}" line is mandantory and gives information about the
+    spectrum name and the precursor mass. $num is counted from 1 as the
+    result block grows. $num is not consectutively increased (mjm: bug?).
+"P{$pepRank}" line is optional which gives information about the matching
+    result of a peptide sequence and its related information, including
+    the peptide rank ($pepRank), the peptide sequence, the PSM score,
+    precursor error. This line is optional which means it could be no
+    "P{$pepRank}" line or multiple lines.
+"time" line is mandantory and indicates the beginning of a result block.
+empty line is always existing before "time" line
+
+After being read into memory, a PSM (Peptide Specturm Match) has below
+structure:
+psm = {
+    "id": "S16-P1",
+    "spectrumTitle": "H1.87.87.2.dta",
+    "peptideSequence": "TKPEWV",
+    "peptideLength": 6,
+    "peptideRank": 1,
+    "score": 5.518731,
+    "precursorMass": 759.400500,
+    "precursorError": 5.518731
+}
+
+Params:
+    filePath the pNovo running result
+    exprimentCondition the expriment condition that could be important to
+                       separate results from multiple groups, such as enzyme.
+                       The value should be a hash and all key-value pairs will
+                       be attached to each PSM. The value is {} by default.
+Return:
+    An array of PSMs
+'''
+def loadpNovoPSMs(filePath, exprimentCondition={}):
+    psms = []
+
+    pepRank = 0
+    spec = {}
+
+    fh = open(filePath)
+    try:
+        lines = fh.readlines()
+    finally:
+        fh.close()
+    for line in lines:
+        line = line.rstrip('\n')
+        if line[0:5] == 'time:' or line is '':
+            continue
+        elif line[0] == 'S':
+            pepRank = 0
+
+            parts = line.split('\t')
+            if len(parts) != 3:
+                raise 'Error length: %s.' % line
+            spec = {
+                "id": parts[0],
+                "title": parts[1],
+                "precursorMass": float(parts[2])
+            }
+        elif line[0] == 'P':
+            pepRank = pepRank + 1
+
+            parts = line.split('\t')
+            if len(parts) != 7:
+                raise 'Error length: %s.' % line
+            if 'P' + str(pepRank) != parts[0]:
+                raise Exception('Error pepRank: expected P%d, actual is %s' \
+                       % (pepRank, parts[0]))
+            psm = {
+                "id": spec["id"] + "-" + parts[0],
+                "spectrumTitle": spec["title"],
+                "peptideSequence": parts[1],
+                "peptideLength": len(parts[1]),
+                "peptideRank": pepRank,
+                "score": float(parts[2]),
+                "precursorMass": spec["precursorMass"],
+                "precursorError": float(parts[6])
+            }
+            for key in exprimentCondition:
+                psm[key] = exprimentCondition[key]
+            psms.append(psm)
+        else:
+            raise 'Error line: %s' % line
+    return psms
+
+def testLoadpNovoPSMs():
+    filePath = 'D:\\Data\\ProteomeDataAnalysis\\Antibody_H\\AnalysisResults\\' \
+               + 'H1\\pNovoSearch\\result\\pNovo.res'
+    exprimentCondition = {
+        "enzyme": "Lys-C"
+    }
+    psms = loadpNovoPSMs(filePath, exprimentCondition)
+    print len(psms)
---
+++ resultSorter.py
@@ -0,0 +1,137 @@
+'''
+sortpNovoPSMs sorts given psms which is a list of PSMs dictionary objects
+according to given method. This method is used for picking better matching
+peptides out of pNovo search results.
+
+Params:
+    psms an array of PSMs. An example of a PSM is like This
+         psm = {
+             "id": "S16-P1",
+             "spectrumTitle": "H1.87.87.2.dta",
+             "peptideSequence": "TKPEWV",
+             "peptideLength": 6,
+             "peptideRank": 1,
+             "score": 5.518731,
+             "precursorMass": 759.400500,
+             "precursorError": 5.518731
+         }
+    method the way to sort the array and produce the sorted result.
+           Possible values are "spectracount", "scorecount". Default is
+           "spectracount".
+           "spectracount" sorts all peptides based on the number of spectra
+           in descending order. If the number of spectra is the same then
+           use the length of the peptides. If the length of peptides is still
+           the same, use the score of the highest PSM. If the score of the
+           highest PSM is still the same, use the alphabetic order of the
+           sequence.
+           "scorecount" sorts all peptides based on the score of the highest
+           PSM in descending order. If the score is the same, use the number
+           of spectra. If the number of spectra is the same, use the length of
+           the peptide. If the length of peptide is still the same, use the
+           alphabetic order of the sequence.
+    conditionKeys the expriment condition that could be important to
+                  separate results from multiple groups, such as enzyme.
+                  The value should be a list and all key-value pairs will
+                  be attached to each PSM. The list is [] by default.
+Return:
+    A sorted array based on the given method. Each item in the array is
+    as follows
+    {
+        "peptideSequence": "TKPEWV",
+        "peptideLength": 6,
+        "peptideRank": 1,
+        "spectraCount": 1,
+        "highestScore": 5.518731,
+        "selectedId": "S16-P1",
+        "selectedTitle": "H1.87.87.2.dta",
+        "selectedPrecursorMass": 759.400500,
+        "selectedPrecursorError": 5.518731,
+        "enzyme": "Lys-C"
+    }
+'''
+def sortpNovoPSMs(psms, method="spectracount", conditionKeys=[]):
+    peptides = convertToPeptideView(psms, conditionKeys)
+    if method.lower() == "spectracount":
+        peptides = multikeysort(peptides, ["-spectraCount", \
+                          "-peptideLength", "-highestScore", \
+                          "peptideSequence"])
+    elif method.lower() == "scorecount":
+        peptides = multikeysort(peptides, ["-highestScore", \
+                          "-spectraCount", "-peptideLength", \
+                          "peptideSequence"])
+    else:
+        errorMsg = ["Unrecognized method type: %s", \
+                    " only support spectracount and scorecount."]
+        raise Exception( ",".join(errorMsg) % method)
+    return peptides
+
+'''
+This method is to sort a list of dictionary by setting multiple keys.
+The idea comes from https://stackoverflow.com/questions/1143671/
+python-sorting-list-of-dictionaries-by-multiple-keys.
+
+multikeysort(peptides, ["-spectraCount", "-peptideLength", "-highestScore", \
+                        "peptideSequence"])
+sorted(peptides, key=lambda peptide: (\
+                  -peptide["spectraCount"], -peptide["peptideLength"],
+                  -peptide["highestScore"], peptide["peptideSequence"])
+'''
+def multikeysort(items, columns):
+    from operator import itemgetter
+    comparers = [((itemgetter(col[1:].strip()), -1) \
+                  if col.startswith('-') \
+                  else (itemgetter(col.strip()), 1)) \
+                  for col in columns]
+    def comparer(left, right):
+        for fn, mult in comparers:
+            result = cmp(fn(left), fn(right))
+            if result:
+                return mult * result
+        else:
+            return 0
+    return sorted(items, cmp=comparer)
+
+def convertToPeptideView(psms, conditionKeys):
+    peptides = {}
+    for psm in psms:
+        if psm["peptideLength"] == 0:
+            continue
+        if psm["peptideSequence"] not in peptides:
+            peptides[psm["peptideSequence"]] = {
+                "peptideSequence": psm["peptideSequence"],
+                "peptideLength": psm["peptideLength"],
+                "peptideRank": 1,
+                "spectraCount": 1,
+                "highestScore": psm["score"],
+                "selectedId": psm["id"],
+                "selectedTitle": psm["spectrumTitle"],
+                "selectedPrecursorMass": psm["precursorMass"],
+                "selectedPrecursorError": psm["precursorError"]
+            }
+            for key in conditionKeys:
+                if key in psm:
+                    peptides[psm["peptideSequence"]][key] = psm[key]
+        else:
+            peptideResult = peptides[psm["peptideSequence"]]
+            peptideResult["spectraCount"] = peptideResult["spectraCount"] + 1
+            if psm["score"] > peptideResult["highestScore"] or \
+               psm["score"] == peptideResult["highestScore"] and \
+               psm["score"] < peptideResult["peptideRank"]:
+                peptideResult["peptideRank"] = psm["peptideRank"]
+                peptideResult["highestScore"] = psm["score"]
+                peptideResult["selectedId"] = psm["id"],
+                peptideResult["selectedTitle"] = psm["spectrumTitle"]
+                peptideResult["selectedPrecursorMass"] = psm["precursorMass"]
+                peptideResult["selectedPrecursorError"] = psm["precursorError"]
+    return [peptide[1] for peptide in peptides.items()]
+
+def testSortpNovoPSMs():
+    from resultLoader import loadpNovoPSMs
+    filePath = 'D:\\Data\\ProteomeDataAnalysis\\Antibody_H\\AnalysisResults\\' \
+               + 'H1\\pNovoSearch\\result\\pNovo.res'
+    exprimentCondition = {
+        "enzyme": "Lys-C"
+    }
+    psms = loadpNovoPSMs(filePath, exprimentCondition)
+    peptides = sortpNovoPSMs(psms)
+    print peptides
